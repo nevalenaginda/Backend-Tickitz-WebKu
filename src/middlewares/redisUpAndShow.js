43f4810baa4350a15fb6034a1f098b1bdbe27078
Nevalen Aginda <nevalenaginda10@gmail.com>
2021-07-05 07:41:11 +0700

    Update redisUpAndShow.js

--- src/middlewares/redisUpAndShow.js
+++ src/middlewares/redisUpAndShow.js
@@ -1,5 +1,5 @@
 const redis = require("redis");
-const client = redis.createClient(6379);
+const client = redis.createClient("/home/nevalena/redis.sock");
 const { response } = require("../helpers/response");
 
 const cacheUpComing = (req, res, next) => {
